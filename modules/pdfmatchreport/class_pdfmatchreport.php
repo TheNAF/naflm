8bbd3a6affee62a5d8e16b1de6896a9518c82afe
daniel.tamajon <dtamajon@litesolutions.es>
2017-10-31 00:03:37 +0100

    Includes name in PDF match report, and some fixes for sizing

--- modules/pdfmatchreport/class_pdfmatchreport.php
+++ modules/pdfmatchreport/class_pdfmatchreport.php
@@ -24,7 +24,7 @@ class PDFMatchReport implements ModuleInterface
         );
     }
 
-  public static function getModuleTables(){return array();}
+    public static function getModuleTables(){return array();}
 
     public static function getModuleUpgradeSQL(){return array();}
 
@@ -55,36 +55,37 @@ class PDFMatchReport implements ModuleInterface
             $team2 = null;
             $match = null;
         }
+
         define("MARGINX", 20);
-	define("MARGINY", 20);
-	define("DEFLINECOLOR", '#000000');
-	define("HEADLINEBGCOLOR", '#c3c3c3');
+        define("MARGINY", 20);
+        define("DEFLINECOLOR", '#000000');
+        define("HEADLINEBGCOLOR", '#c3c3c3');
 
-	// Custom settings for inducements.
+        // Custom settings for inducements.
 
-	define('MAX_STARS', 2);
-	define('MERC_EXTRA_COST', 30000);
-	define('MERC_EXTRA_SKILL_COST', 50000);
+        define('MAX_STARS', 2);
+        define('MERC_EXTRA_COST', 30000);
+        define('MERC_EXTRA_SKILL_COST', 50000);
 
-	// Color codes.
-	define('COLOR_ROSTER_NORMAL',   COLOR_HTML_NORMAL);
-	define('COLOR_ROSTER_READY',    COLOR_HTML_READY);
-	define('COLOR_ROSTER_MNG',      COLOR_HTML_MNG);
-	define('COLOR_ROSTER_DEAD',     COLOR_HTML_DEAD);
-	define('COLOR_ROSTER_SOLD',     COLOR_HTML_SOLD);
-	define('COLOR_ROSTER_STARMERC', COLOR_HTML_STARMERC);
-	define('COLOR_ROSTER_JOURNEY',  COLOR_HTML_JOURNEY);
-	define('COLOR_ROSTER_JOURNEY_USED',  COLOR_HTML_JOURNEY_USED);
-	define('COLOR_ROSTER_NEWSKILL', COLOR_HTML_NEWSKILL);
-	//-----
-	define('COLOR_ROSTER_CHR_EQP1', COLOR_HTML_CHR_EQP1); // Characteristic equal plus one.
-	define('COLOR_ROSTER_CHR_GTP1', COLOR_HTML_CHR_GTP1); // Characteristic greater than plus one.
-	define('COLOR_ROSTER_CHR_EQM1', COLOR_HTML_CHR_EQM1); // Characteristic equal minus one.
-	define('COLOR_ROSTER_CHR_LTM1', COLOR_HTML_CHR_LTM1); // Characteristic less than minus one.
+        // Color codes.
+        define('COLOR_ROSTER_NORMAL',   COLOR_HTML_NORMAL);
+        define('COLOR_ROSTER_READY',    COLOR_HTML_READY);
+        define('COLOR_ROSTER_MNG',      COLOR_HTML_MNG);
+        define('COLOR_ROSTER_DEAD',     COLOR_HTML_DEAD);
+        define('COLOR_ROSTER_SOLD',     COLOR_HTML_SOLD);
+        define('COLOR_ROSTER_STARMERC', COLOR_HTML_STARMERC);
+        define('COLOR_ROSTER_JOURNEY',  COLOR_HTML_JOURNEY);
+        define('COLOR_ROSTER_JOURNEY_USED',  COLOR_HTML_JOURNEY_USED);
+        define('COLOR_ROSTER_NEWSKILL', COLOR_HTML_NEWSKILL);
+        //-----
+        define('COLOR_ROSTER_CHR_EQP1', COLOR_HTML_CHR_EQP1); // Characteristic equal plus one.
+        define('COLOR_ROSTER_CHR_GTP1', COLOR_HTML_CHR_GTP1); // Characteristic greater than plus one.
+        define('COLOR_ROSTER_CHR_EQM1', COLOR_HTML_CHR_EQM1); // Characteristic equal minus one.
+        define('COLOR_ROSTER_CHR_LTM1', COLOR_HTML_CHR_LTM1); // Characteristic less than minus one.
 
-	define('T_PDF_ROSTER_SET_EMPTY_ON_ZERO', true); # Prints cp, td etc. as '' (empty string) when field = 0.
+        define('T_PDF_ROSTER_SET_EMPTY_ON_ZERO', true); # Prints cp, td etc. as '' (empty string) when field = 0.
 
-	$ind_cost=0;
+        $ind_cost=0;
 
         $pdf=new BB_PDF('L','pt','A4'); // Creating a new PDF doc. landscape, scale=pixels, size A4
         $pdf->SetAutoPageBreak(false, 20); // No auto page break to mess up layout
@@ -140,22 +141,22 @@ class PDFMatchReport implements ModuleInterface
         }
 
 
-		$currentx += 190;
-		$currenty += 15;
-		$pdf->SetXY($currentx,$currenty);
-		$pdf->SetFont('Tahoma','B',12);
-		$noname = '__________________';
-		$norace = '__________________';
-		$pdf->Cell(390, 50, (!$FILLED ? $noname : $team1->name).' - '.(!$FILLED ? $noname : $team2->name), 0, 0, 'C', false, '');
-		$currenty += 15;
-		$pdf->SetXY($currentx,$currenty);
-		$pdf->SetFont('Tahoma','',10);
-		$pdf->Cell(390, 50, (!$FILLED ? $norace : $team1->f_rname).' - '.(!$FILLED ? $norace : $team2->f_rname), 0, 0, 'C', false, '');
+        $currentx += 190;
+        $currenty += 15;
+        $pdf->SetXY($currentx,$currenty);
+        $pdf->SetFont('Tahoma','B',12);
+        $noname = '__________________';
+        $norace = '__________________';
+        $pdf->Cell(390, 50, (!$FILLED ? $noname : $team1->name).' - '.(!$FILLED ? $noname : $team2->name), 0, 0, 'C', false, '');
+        $currenty += 15;
+        $pdf->SetXY($currentx,$currenty);
+        $pdf->SetFont('Tahoma','',10);
+        $pdf->Cell(390, 50, (!$FILLED ? $norace : $team1->f_rname).' - '.(!$FILLED ? $norace : $team2->f_rname), 0, 0, 'C', false, '');
 
 
-	// Gate + score text
-		$pdf->SetFont('Tahoma','',10);
-		$space = '        ';
+    // Gate + score text
+        $pdf->SetFont('Tahoma','',10);
+        $space = '        ';
         $currenty += 26;
         $pdf->SetXY($currentx,$currenty);
         $pdf->Cell(210, 50, "Score:       -", 0, 0, 'R', false, '');
@@ -164,10 +165,10 @@ class PDFMatchReport implements ModuleInterface
         $pdf->Cell(210, 50, "Gate:${space}k", 0, 0, 'R', false, '');
 
        //Printing headers for player rows. Do all this twice
-        $smallFieldSize = 25;
-        $mediumFieldSize = 90;
+        $smallFieldSize = 20;
+        $mediumFieldSize = 80;
         $nameFieldSize = 100;
-        $skillFieldSize = 350;
+        $skillFieldSize = 370;
 
 
         //Print two team boxes
@@ -178,7 +179,7 @@ class PDFMatchReport implements ModuleInterface
             $i++;
 
             $currenty += 15;
-            $currentx = MARGINX + 6;
+            $currentx = MARGINX - 20;
             $pdf->SetXY($currentx,$currenty);
 
             $pdf->SetFillColorBB($pdf->hex2cmyk('#c6c6c6'));
@@ -269,11 +270,12 @@ class PDFMatchReport implements ModuleInterface
 
             // Printing headline for player table
             $pdf->Cell($smallFieldSize, $h, 'Nr', 1, 0, 'C', true, '');
+            $pdf->Cell($mediumFieldSize, $h, 'Name', 1, 0, 'C', true, '');
             $pdf->Cell($mediumFieldSize, $h, 'Position', 1, 0, 'C', true, '');
-	        $pdf->Cell($smallFieldSize, $h, 'MA', 1, 0, 'C', true, '');
-	        $pdf->Cell($smallFieldSize, $h, 'ST', 1, 0, 'C', true, '');
-	        $pdf->Cell($smallFieldSize, $h, 'AG', 1, 0, 'C', true, '');
-	        $pdf->Cell($smallFieldSize, $h, 'AV', 1, 0, 'C', true, '');
+            $pdf->Cell($smallFieldSize, $h, 'MA', 1, 0, 'C', true, '');
+            $pdf->Cell($smallFieldSize, $h, 'ST', 1, 0, 'C', true, '');
+            $pdf->Cell($smallFieldSize, $h, 'AG', 1, 0, 'C', true, '');
+            $pdf->Cell($smallFieldSize, $h, 'AV', 1, 0, 'C', true, '');
             $pdf->Cell($skillFieldSize, $h, 'Skills', 1, 0, 'L', true, '');
             $pdf->Cell($smallFieldSize, $h, 'SPP', 1, 0, 'C', true, '');
             $pdf->Cell($smallFieldSize, $h, 'MVP', 1, 0, 'C', true, '');
@@ -323,62 +325,60 @@ class PDFMatchReport implements ModuleInterface
                 $spp = '';
                 $bgc = COLOR_ROSTER_NORMAL;
                 if (count($players) >= $j) {
-                  $p = $players[$j-1];
+                    $p = $players[$j-1];
 
-                  $name = $p->name;
-                  $pos = $p->position;  //Get position
-                  $bgc = COLOR_ROSTER_NORMAL;
-                  if ($p->is_journeyman) {
-                       $name = "$p->name [J]";
-                       $bgc = COLOR_ROSTER_JOURNEY;
-                  }
-                  if ($p->is_journeyman_used) {
-                       $name = "$p->name [J]";
-                       $bgc = COLOR_ROSTER_JOURNEY_USED;
-                  }
-                  if ($p->is_mng) {
-                       $name = "$p->name [MNG]";
-                       $bgc = COLOR_ROSTER_MNG;
-                  }
-                  if ($p->mayHaveNewSkill()) {
-                       $name = "$p->name";
-                       $bgc = COLOR_ROSTER_NEWSKILL;
-                  }
-
-
-                  $nr = $p->nr;
-                  $ma = $p->ma;
-                  $st = $p->st;
-                  $ag = $p->ag;
-                  $av = $p->av;
-
-                  //  Get skills and injuries
-                  $skillstr = $p->getSkillsStr(false);
-                  $injstr = $p->getInjsStr(false);
-                  if ($skillstr == '') {  // No skills
-                       if ($injstr != '') $skills_injuries=$injstr;  // Only injuries
-                      else $skills_injuries=''; // No skills nor injuries
+                    $name = $p->name;
+                    $pos = $p->position;  //Get position
+                    $bgc = COLOR_ROSTER_NORMAL;
+                    if ($p->is_journeyman) {
+                         $name = "$p->name [J]";
+                         $bgc = COLOR_ROSTER_JOURNEY;
                     }
-                  else {
-                 if ($injstr != '') $skills_injuries=$skillstr . ', ' . $injstr;   // Skills and injuries separated with ', '
-                 else $skills_injuries=$skillstr;  // Only skills, no injuries
-                  }
+                    if ($p->is_journeyman_used) {
+                         $name = "$p->name [J]";
+                         $bgc = COLOR_ROSTER_JOURNEY_USED;
+                    }
+                    if ($p->is_mng) {
+                         $name = "$p->name [MNG]";
+                         $bgc = COLOR_ROSTER_MNG;
+                    }
+                    if ($p->mayHaveNewSkill()) {
+                         $name = "$p->name";
+                         $bgc = COLOR_ROSTER_NEWSKILL;
+                    }
 
-                  $spp = $p->mv_spp;  //Current Star Player Points
+
+                    $nr = $p->nr;
+                    $ma = $p->ma;
+                    $st = $p->st;
+                    $ag = $p->ag;
+                    $av = $p->av;
+
+                    //  Get skills and injuries
+                    $skillstr = $p->getSkillsStr(false);
+                    $injstr = $p->getInjsStr(false);
+                    if ($skillstr == '') {  // No skills
+                       if ($injstr != '') $skills_injuries=$injstr;  // Only injuries
+                       else $skills_injuries=''; // No skills nor injuries
+                    }
+                    else {
+                        if ($injstr != '') $skills_injuries=$skillstr . ', ' . $injstr;   // Skills and injuries separated with ', '
+                        else $skills_injuries=$skillstr;  // Only skills, no injuries
+                    }
+
+                    $spp = $p->mv_spp;  //Current Star Player Points
                 }
 
-
-
-               $pdf->SetFillColorBB($pdf->hex2cmyk($bgc));
-
+                $pdf->SetFillColorBB($pdf->hex2cmyk($bgc));
 
                 $pdf->Cell($smallFieldSize, $h, $nr, 1, 0, 'C', true, '');
+                $pdf->Cell($mediumFieldSize, $h, utf8_decode($name), 1, 0, 'C', true, '');
                 $pdf->Cell($mediumFieldSize, $h, $pos, 1, 0, 'C', true, '');
                 $pdf->Cell($smallFieldSize, $h, $ma, 1, 0, 'C', true, '');
-	            $pdf->Cell($smallFieldSize, $h, $st, 1, 0, 'C', true, '');
-	            $pdf->Cell($smallFieldSize, $h, $ag, 1, 0, 'C', true, '');
-	            $pdf->Cell($smallFieldSize, $h, $av, 1, 0, 'C', true, '');
-                $pdf->Cell($skillFieldSize, $h, $skills_injuries, 1, 0, 'L', true, '');
+                $pdf->Cell($smallFieldSize, $h, $st, 1, 0, 'C', true, '');
+                $pdf->Cell($smallFieldSize, $h, $ag, 1, 0, 'C', true, '');
+                $pdf->Cell($smallFieldSize, $h, $av, 1, 0, 'C', true, '');
+                $pdf->Cell($skillFieldSize, $h, utf8_decode($skills_injuries), 1, 0, 'L', true, '');
                 $pdf->Cell($smallFieldSize, $h, $spp, 1, 0, 'C', true, '');
                 $pdf->Cell($smallFieldSize, $h, '', 1, 0, 'C', true, '');
                 $pdf->Cell($smallFieldSize, $h, '', 1, 0, 'C', true, '');
@@ -389,11 +389,10 @@ class PDFMatchReport implements ModuleInterface
                 $pdf->Cell($smallFieldSize, $h, '', 1, 0, 'C', true, '');
                 $pdf->Cell($smallFieldSize, $h, '', 1, 0, 'C', true, '');
 
+                $name="";
                 $currenty+=$h;
                 $pdf->SetXY($currentx - 20,$currenty);
-
             }
-
 
             // Color legends
             $pdf->SetFont('Tahoma', '', 7);
@@ -420,11 +419,9 @@ class PDFMatchReport implements ModuleInterface
             $pdf->SetXY($currentx+=5, $currenty-=1);
             $pdf->Cell(70, 8, 'New skill available', 0, 0, 'L', false);
 
-
         }
 
         // Output the PDF document
         $pdf->Output("Match Report.pdf", 'I');
     }
-
 }
